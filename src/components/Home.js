8827a983694d4241fe4b3d938fb074a71fec0b30
Abdurehman05 <abdukr5@gmail.com>
2020-10-20 15:50:59 -0500

    home componenet

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.js";
+import "./App.css";
+
+export const Home = () => (
+  <>
+    <div className="homeHeader">
+      <h1>Welcome</h1>
+    </div>
+  </>
+);
