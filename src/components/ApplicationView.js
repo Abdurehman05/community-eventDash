309c2ec3fc7fdb95c39d0281bfd235027e90e249
Abdurehman05 <abdukr5@gmail.com>
2020-10-20 16:00:25 -0500

    applicationView component created

---
+++ src/components/ApplicationView.js
@@ -0,0 +1,12 @@
+ApplicationViews;
+import React from "react";
+import { Route } from "react-router-dom";
+import { Home } from "./Home";
+
+export const ApplicationViews = props => {
+  return (
+    <Route exact path="/">
+      <Home />
+    </Route>
+  );
+};
